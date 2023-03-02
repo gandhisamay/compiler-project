c9e3c6c8e01390e78edcdd937a5873400e1c95da
Samay Gandhi <gandhisamay200@gmail.com>
2023-03-02 19:33:29 +0530

    final code ready for submission

--- src/test.sh
+++
@@ -1,6 +0,0 @@
-./a.out ../tests/test_cases_stage_1/t6_syntax_errors.txt test.txt 100
-./a.out ../tests/test_cases_stage_1/t2.txt test.txt 100
-./a.out ../tests/test_cases_stage_1/t1.txt test.txt 100
-./a.out ../tests/test_cases_stage_1/t5.txt test.txt 100
-./a.out ../tests/test_cases_stage_1/t3.txt test.txt 100
-./a.out ../tests/test_cases_stage_1/t4.txt test.txt 100
